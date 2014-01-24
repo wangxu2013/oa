9a96ae46f314ba0406e12a08e68faa85d7db3f36
tanwh <qk_tanwh@163.com>
2014-01-23 21:10:19 -0800

    [mod]财务看到所有公司报销 [mod]财务只看到审批界面

--- scapp/views/reimbursement/oa_reimbursement.py
+++ scapp/views/reimbursement/oa_reimbursement.py
@@ -115,13 +115,17 @@ def get_fybx_check_query(page,return_type):
         for obj in chileDepartment:
             sql+=","+str(obj.id)
         sql+=")"
-
+    
+    #财务能看到所有公司报销
+    if level == 5:
+        sql = ""
+        
     if return_type:
         if return_type=='json':
-            data=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<:status","init_level<:role_level").params(status=status,role_level=level).order_by("id").all()
+            data=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<=:status","init_level<:role_level").params(status=status,role_level=level).order_by("id").all()
             return json.dumps(data,cls=DateDecimalEncoder,ensure_ascii=False)
         else:
-            data=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<:status","init_level<:role_level").params(status=status,role_level=level).order_by("id").paginate(page, per_page = PER_PAGE)
+            data=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<=:status","init_level<:role_level").params(status=status,role_level=level).order_by("id").paginate(page, per_page = PER_PAGE)
             return render_template("bxsq/check_list.html",data=data,role=role)
 
 #费用审批
