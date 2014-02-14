00525158e83e72a5b566f3132b8f1b5464ade53c
tanwh <qk_tanwh@163.com>
2014-02-13 23:06:44 -0800

    [mod]增加财务总监，修改相应逻辑

--- scapp/logic/reimbursement/costs_statistics.py
+++ scapp/logic/reimbursement/costs_statistics.py
@@ -5,15 +5,23 @@ from scapp import db
 
 
 def get_total_apply_costs(org_id):
-    str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement WHERE org_id=%s"%(org_id)
+    if org_id != -1:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement WHERE org_id=%s"%(org_id)
+    else:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement"
+
     result=db.session.execute(str_query_string).fetchall()
     for data in result:
         total_amount=data['t_amount']
-    return total_amount
+    return total_amount if total_amount is not None else 0
 
 
 def get_total_paid_costs(org_id):
-    str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement WHERE is_paid='1' AND org_id=%s"%(org_id)
+    if org_id != -1:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement WHERE is_paid='1' AND org_id=%s"%(org_id)
+    else:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement WHERE is_paid='1'"
+    
     result=db.session.execute(str_query_string).fetchall()
     for data in result:
         total_amount=data['t_amount']
@@ -21,9 +29,15 @@ def get_total_paid_costs(org_id):
 
 
 def get_monthly_paid_costs(org_id):
-    str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
+    if org_id != -1:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
                      "WHERE is_paid='1' AND month(paid_date) =month(curdate()) " \
                      "AND year(paid_date) = year(curdate()) AND org_id=%s"%(org_id)
+    else:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
+                     "WHERE is_paid='1' AND month(paid_date) =month(curdate()) " \
+                     "AND year(paid_date) = year(curdate())"
+
     result=db.session.execute(str_query_string).fetchall()
     for data in result:
         total_amount=data['t_amount']
@@ -31,9 +45,14 @@ def get_monthly_paid_costs(org_id):
 
 
 def get_season_paid_costs(org_id):
-    str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
+    if org_id != -1:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
                      "WHERE is_paid='1' AND quarter( FROM_UNIXTIME( paid_date ) ) = quarter( curdate( )) " \
                      " AND org_id=%s"%(org_id)
+    else:
+        str_query_string="SELECT SUM(amount) AS t_amount FROM oa_reimbursement " \
+                     "WHERE is_paid='1' AND quarter( FROM_UNIXTIME( paid_date ) ) = quarter( curdate( )) "
+
     result=db.session.execute(str_query_string).fetchall()
     for data in result:
         total_amount=data['t_amount']
--- scapp/views/index.py
+++ scapp/views/index.py
@@ -34,7 +34,7 @@ def login():
             count_2 = 0 #待您审批
 
             status = 1
-            if level == 4 or level == 5:
+            if level == 4 or level == 5 or level == 6:
                 status = level - 1
 
             departmentId=current_user.department
@@ -49,12 +49,14 @@ def login():
                 sql+=")"
 
             #财务能看到所有公司报销
-            if level == 5:
+            if level == 5 or level == 6:
                 sql = ""
 
-            count_1=OA_Reimbursement.query.filter("create_user=:id","is_refuse=0","is_retreat=0","status!=5").params(id=current_user.id).count()
+            count_1=OA_Reimbursement.query.filter("create_user=:id","is_refuse=0","is_retreat=0","status!=6").params(id=current_user.id).count()
 
-            if level == 5:
+            if level == 6:
+                count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status=5","init_level<:role_level").params(role_level=level).count()
+            elif level == 5:
                 count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status=4","init_level<:role_level").params(role_level=level).count()
             else:
                 count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<=:status","init_level<:role_level").params(status=status,role_level=level).count()
@@ -84,11 +86,33 @@ def welcome():
     count_1 = 0 #待上级审批
     count_2 = 0 #待您审批
 
-    if level == 4 or level == 5:
+    if level == 4 or level == 5 or level == 6:
         status = level - 1
 
-    count_1=OA_Reimbursement.query.filter("create_user=:id","is_refuse=0","is_retreat=0","status!=5").params(id=current_user.id).count()
-    count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0","org_id=:department","status<:status","init_level<:role_level").params(department=current_user.department,status=status,role_level=level).count()
+    departmentId=current_user.department
+    departmentLevel=OA_Org.query.filter_by(id=departmentId).first().org_level
+
+    sql="org_id="+str(departmentId)
+    if departmentLevel == 1:#公司
+        chileDepartment=OA_Org.query.filter_by(pId=departmentId).all()
+        sql="org_id in ("+str(departmentId)
+        for obj in chileDepartment:
+            sql+=","+str(obj.id)
+        sql+=")"
+
+    #财务能看到所有公司报销
+    if level == 5 or level == 6:
+        sql = ""
+
+    count_1=OA_Reimbursement.query.filter("create_user=:id","is_refuse=0","is_retreat=0","status!=6").params(id=current_user.id).count()
+
+    if level == 6:
+        count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status=5","init_level<:role_level").params(role_level=level).count()
+    elif level == 5:
+        count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status=4","init_level<:role_level").params(role_level=level).count()
+    else:
+        count_2=OA_Reimbursement.query.filter("is_refuse=0","is_retreat=0",sql,"status<=:status","init_level<:role_level").params(status=status,role_level=level).count()
+
     return render_template("welcome.html",role=role,count_1=count_1,count_2=count_2)
 
 # 系统管理
